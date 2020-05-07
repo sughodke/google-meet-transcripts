b63a3962fc8a731b7d310d5d3fc67735566029de
Matthew Dzaman <matthew@dzaman.org>
2020-05-06 22:31:50 -0700

    🎉 New icon, 🛠 move extension to src

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,7 @@ try {
   const node = document.getElementsByTagName('html')[0];
   const script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
-  const filePath = chrome.runtime.getURL('lib/stenographer.js');
+  const filePath = chrome.runtime.getURL('stenographer.js');
   script.setAttribute('src', filePath)
   node.appendChild(script);
 } catch (e) {
